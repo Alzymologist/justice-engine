9cb5c744172b4eca0740ab07fe46bb960c158be9
Evgenii-Barannik <radchemsu@gmail.com>
2023-01-20 17:30:21 +0200

    Hash is calculated from a serialized string.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use bs58;
 use serde_json;
 use tiny_keccak::{IntoXof, KangarooTwelve, Xof};
 
-const KANGAROO_OUTPUT_SIZE: usize = 8; // Not equal to .hash size in SerializedLaw
+const KANGAROO_OUTPUT_SIZE: usize = 8; // Not equal to hash size in SerializedLaw
 
 struct SerializedLaw {
     law: String,
@@ -10,15 +10,17 @@ struct SerializedLaw {
 }
 
 fn serialize(raw_law: String) -> SerializedLaw {
-    let bytes = &raw_law.into_bytes();
-    let mut hasher = KangarooTwelve::new(bytes).into_xof();
+    let bytes = raw_law.into_bytes();
+    let serialized_law = serde_json::to_string(&bytes).unwrap();  // String serialization.
+
+    let serialized_law_clone = serialized_law.clone();
+    let mut hasher = KangarooTwelve::new(serialized_law.into_bytes()).into_xof(); // Hash is calculated from a serialized string.
     let mut output = [0u8; KANGAROO_OUTPUT_SIZE];
     hasher.squeeze(&mut output[..]);
 
     let hash = bs58::encode(output).into_string(); // Base 58 for better visual representation.
-    let law = serde_json::to_string(bytes).unwrap();
 
-    SerializedLaw { law, hash }
+    SerializedLaw { law: serialized_law_clone, hash }
 }
 
 fn main() {
