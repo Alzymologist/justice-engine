c8518da63c53ab917e8ee5451fd263ce84f45322
Evgenii-Barannik <radchemsu@gmail.com>
2023-01-28 01:34:32 +0200

    Is it really working?

--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,25 @@ fn read_yaml(path: &Path) -> String {
     }
 }
 
-fn sanitize_tree(
-    hash_to_traverse: &LinkedHashMap<Yaml, Yaml>,
-    hash_to_modify: &mut LinkedHashMap<Yaml, Yaml>, // This tree should be a copy initially.
-) {
-    for (key, value) in hash_to_traverse.iter() {
+fn sanitize_tree(hashmap_to_modify: &mut LinkedHashMap<Yaml, Yaml>)
+ {
+    let hashmap_to_traverse = hashmap_to_modify.to_owned();
+    for (key, value) in hashmap_to_traverse.iter() {
+        println!("initial:\n{:?}", hashmap_to_modify[&key]);
         match value {
             Yaml::Real(initial) => {
                 let numeric: f64 = initial.parse().expect("Failed to parse Real.");
                 let sanitized = numeric.to_string();
-                hash_to_modify.insert(key.to_owned(), Yaml::Real(sanitized));
+                hashmap_to_modify.insert(key.to_owned(), Yaml::Real(sanitized));
+            }
+            Yaml::Hash(deeper_hashmap_to_traverse) => {
+
+                if let Yaml::Hash(mut hashmap2) = hashmap_to_modify[&key].to_owned(){
+                sanitize_tree(&mut hashmap2);
+                }
+
+                // let mut deeper_hashmap_to_modify = &hashmap_to_modify[&key];
+                // sanitize_tree(deeper_hashmap_to_modify);
             }
             _ => (),
         }
@@ -47,16 +56,21 @@ fn main() {
     let yaml_string = read_yaml(&path);
     let docs = YamlLoader::load_from_str(&yaml_string).unwrap();
 
-    if let Yaml::Hash(hash) = &docs[0] {
-        println!("initial_hashtable:\n{:?}", hash);
-        let mut hash_to_mod = hash.to_owned();
-        sanitize_tree(hash, &mut hash_to_mod);
+    if let Yaml::Hash(hashmap) = &docs[0] {
+        println!("initial_hashtable:\n{:?}", hashmap);
+        let mut hashmap_to_mod = hashmap.to_owned();
+        sanitize_tree(&mut hashmap_to_mod);
+        //
+        let s1 = Yaml::String("foo".to_string());
+        let h1 = Yaml::String("test1".to_string());
+        let h2 = &hashmap[&h1].as_hash();
+        println!("access:\n{:?}", h2);
 
         {
-            let hash_to_mod_clone = hash_to_mod.clone(); // For printing
+            let hash_to_mod_clone = hashmap_to_mod.clone(); // For printing
             let mut s = String::new();
             let mut emitter = YamlEmitter::new(&mut s);
-            emitter.dump(&Yaml::Hash(hash_to_mod)).unwrap(); // Dump the YAML to a String
+            emitter.dump(&Yaml::Hash(hashmap_to_mod)).unwrap(); // Dump the YAML to a String
 
             let mut hasher = Blake2s256::new();
             hasher.update(s);
