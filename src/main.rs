70c1b5466aefc9e3afdc9707f7031d2a4710b30a
Evgenii-Barannik <radchemsu@gmail.com>
2023-03-30 02:59:28 +0300

    Compiles to wasm, but empty site

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use yew::prelude::*;
-use base64::{Engine, engine::general_purpose};
+use base64::{engine::general_purpose, Engine};
 use reqwest::Client;
 use serde::Deserialize;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
-use tokio;
+use wasm_bindgen_futures::spawn_local;
+use yew::functional::{use_effect, use_state};
+use yew::prelude::*;
 
 #[derive(Deserialize)]
 struct Config {
@@ -22,79 +23,52 @@ fn read_config() -> Result<Config, Box<dyn Error>> {
     Ok(config)
 }
 
-fn request_tree() -> Result<String, Box<dyn Error>> {
+async fn request_tree() -> Result<String, Box<dyn Error>> {
     let config = read_config()?;
 
-    let rt = tokio::runtime::Runtime::new()?;
-    let response2_text = rt.block_on(async {
-        let auth_header = format!(
-            "Basic {}",
-            general_purpose::STANDARD.encode(&format!("{}:{}", config.PROJECT_ID, config.PROJECT_SECRET))
-        );
-        let client = Client::builder()
-            .default_headers({
-                let mut headers = reqwest::header::HeaderMap::new();
-                headers.insert("Authorization", auth_header.parse()?);
-                headers
-            })
-            .build()?;
+    let auth_header = format!(
+        "Basic {}",
+        general_purpose::STANDARD
+            .encode(&format!("{}:{}", config.PROJECT_ID, config.PROJECT_SECRET))
+    );
+    let client = Client::builder()
+        .default_headers({
+            let mut headers = reqwest::header::HeaderMap::new();
+            headers.insert("Authorization", auth_header.parse()?);
+            headers
+        })
+        .build()?;
 
-        // Define the hash value here
-        let hash = "QmfUwJRRDZxGo8jMvKVGxj6FDn8xsMXcyEbRrYaScCXhRv";
+    let hash = "QmfUwJRRDZxGo8jMvKVGxj6FDn8xsMXcyEbRrYaScCXhRv";
 
-        // READ FILE WITH HASH
-        let response2 = client
-            .post(&format!("{}/api/v0/cat", config.ENDPOINT))
-            .query(&[("arg", hash)])
-            .send()
-            .await?;
-        println!("{:?}", response2);
-        let response2_text = response2.text().await?;
-
-        Result::<String, Box<dyn Error>>::Ok(response2_text)
-    })?;
-
+    // READ FILE WITH HASH
+    let response2 = client
+        .post(&format!("{}/api/v0/cat", config.ENDPOINT))
+        .query(&[("arg", hash)])
+        .send()
+        .await?;
+    println!("{:?}", response2);
+    let response2_text = response2.text().await?;
     Ok(response2_text)
 }
 
-    //////
-
-
-// use gloo_net::http::Request;
-// use yaml_rust::{Yaml, YamlLoader};
-// use tokio::runtime::Builder;
-
-// fn get_yamls() -> Vec<Yaml> {
-
-//     let rt = Builder::new_current_thread()
-//         .build()
-//         .unwrap();
-
-//     let request = Request::get("http://127.0.0.1:8081/ipfs/QmfUwJRRDZxGo8jMvKVGxj6FDn8xsMXcyEbRrYaScCXhRv");
-
-//     let fetched_yamls = rt.block_on(async {
-//         let response_text = request
-//             .send()
-//             .await
-//             .unwrap()
-//             .text()
-//             .await
-//             .unwrap();
-//         YamlLoader::load_from_str(&response_text).unwrap()
-//     });
-//     fetched_yamls
-// }
-
-
 #[function_component(App)]
 fn app() -> Html {
-    let string_to_render = match request_tree() {
-        Ok(s) => {s},
-        Err(e) => {e.to_string() }};
-
+    let response_state = use_state(|| String::from("Loading..."));
+    {
+        let response_state = response_state.clone();
+            spawn_local(async move {
+                let result = request_tree().await;
+                let message = match result {
+                    Ok(s) => s,
+                    Err(e) => e.to_string(),
+                };
+                response_state.set(message);
+            });
+    }
     html! {
         <>
-        {format!("{:?}", string_to_render)}
+        {format!("{:?}", response_state)}
         </>
     }
 }
