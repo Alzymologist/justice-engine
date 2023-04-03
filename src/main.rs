da3ea38810b752073e1eb8458bc0e3abafc95095
Evgenii-Barannik <radchemsu@gmail.com>
2023-04-04 02:31:31 +0300

    May compile to wasm, can't check without yew

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,47 @@
 use base64::{Engine, engine::general_purpose};
-use reqwest::Client;
-use std::error::Error;
-use tokio;
+use reqwasm::http::{Request, Method};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
+use log::{info, error};
 
 const PROJECT_ID: &str = "2NdtdwZgrdj6fwMubLQMiMTs5nH";
 const PROJECT_SECRET: &str = "78c624acbfe219c5d0b4a8566c867ab0";
 const ENDPOINT: &str = "https://ipfs.infura.io:5001";
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let rt = tokio::runtime::Runtime::new()?;
-    rt.block_on(async {
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    console_log::init_with_level(log::Level::Debug).expect("Error initializing console_log");
+
+    spawn_local(async {
         let auth_header = format!(
             "Basic {}",
             general_purpose::STANDARD.encode(&format!("{}:{}", PROJECT_ID, PROJECT_SECRET))
         );
-        let client = Client::builder()
-            .default_headers({
-                let mut headers = reqwest::header::HeaderMap::new();
-                headers.insert("Authorization", auth_header.parse()?);
-                headers
-            })
-            .build()?;
 
         // Define the hash value here
         let hash = "QmfUwJRRDZxGo8jMvKVGxj6FDn8xsMXcyEbRrYaScCXhRv";
 
         // READ FILE WITH HASH
-        let response2 = client
-            .post(&format!("{}/api/v0/cat", ENDPOINT))
-            .query(&[("arg", hash)])
+        let response = Request::new(&format!("{}/api/v0/cat?arg={}", ENDPOINT, hash))
+            .method(Method::POST)
+            .header("Authorization", &auth_header)
             .send()
-            .await?;
-        println!("{:?}", response2);
-        let response2_text = response2.text().await?;
-        println!("{}", response2_text);
+            .await;
 
-        Result::<_, Box<dyn Error>>::Ok(())
-    })
+        match response {
+            Ok(response) => {
+                if response.status() == 200 {
+                    let text = response.text().await.unwrap();
+                    info!("File content: {}", text);
+                } else {
+                    error!("Error: {:?}", response.status());
+                }
+            }
+            Err(err) => {
+                error!("Error: {:?}", err);
+            }
+        }
+    });
+
+    Ok(())
 }
