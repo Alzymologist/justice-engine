9034e432d680df508ebfcf600a02cbb9eafd54ed
Evgenii-Barannik <radchemsu@gmail.com>
2023-04-03 23:59:59 +0300

    Local ipfs with infura, but without file reading
    std::fs::File and std::io::Read might interfere with wasm compilation

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,40 @@
-use base64::{engine::general_purpose, Engine};
+use base64::{Engine, engine::general_purpose};
 use reqwest::Client;
-use serde::Deserialize;
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
-use wasm_bindgen_futures::spawn_local;
-use yew::functional::{use_effect, use_state};
-use yew::prelude::*;
+use tokio;
 
-#[derive(Deserialize)]
-struct Config {
-    PROJECT_ID: String,
-    PROJECT_SECRET: String,
-    ENDPOINT: String,
-}
+const PROJECT_ID: &str = "2NdtdwZgrdj6fwMubLQMiMTs5nH";
+const PROJECT_SECRET: &str = "78c624acbfe219c5d0b4a8566c867ab0";
+const ENDPOINT: &str = "https://ipfs.infura.io:5001";
 
-fn read_config() -> Result<Config, Box<dyn Error>> {
-    let mut file = File::open("keys.json")?;
-    let mut data = String::new();
-    file.read_to_string(&mut data)?;
-    let config: Config = serde_json::from_str(&data)?;
-    Ok(config)
-}
+fn main() -> Result<(), Box<dyn Error>> {
+    let rt = tokio::runtime::Runtime::new()?;
+    rt.block_on(async {
+        let auth_header = format!(
+            "Basic {}",
+            general_purpose::STANDARD.encode(&format!("{}:{}", PROJECT_ID, PROJECT_SECRET))
+        );
+        let client = Client::builder()
+            .default_headers({
+                let mut headers = reqwest::header::HeaderMap::new();
+                headers.insert("Authorization", auth_header.parse()?);
+                headers
+            })
+            .build()?;
 
-async fn request_tree() -> Result<String, Box<dyn Error>> {
-    let config = read_config()?;
+        // Define the hash value here
+        let hash = "QmfUwJRRDZxGo8jMvKVGxj6FDn8xsMXcyEbRrYaScCXhRv";
 
-    let auth_header = format!(
-        "Basic {}",
-        general_purpose::STANDARD
-            .encode(&format!("{}:{}", config.PROJECT_ID, config.PROJECT_SECRET))
-    );
-    let client = Client::builder()
-        .default_headers({
-            let mut headers = reqwest::header::HeaderMap::new();
-            headers.insert("Authorization", auth_header.parse()?);
-            headers
-        })
-        .build()?;
+        // READ FILE WITH HASH
+        let response2 = client
+            .post(&format!("{}/api/v0/cat", ENDPOINT))
+            .query(&[("arg", hash)])
+            .send()
+            .await?;
+        println!("{:?}", response2);
+        let response2_text = response2.text().await?;
+        println!("{}", response2_text);
 
-    let hash = "QmfUwJRRDZxGo8jMvKVGxj6FDn8xsMXcyEbRrYaScCXhRv";
-
-    // READ FILE WITH HASH
-    let response2 = client
-        .post(&format!("{}/api/v0/cat", config.ENDPOINT))
-        .query(&[("arg", hash)])
-        .send()
-        .await?;
-    println!("{:?}", response2);
-    let response2_text = response2.text().await?;
-    Ok(response2_text)
-}
-
-#[function_component(App)]
-fn app() -> Html {
-    let response_state = use_state(|| String::from("Loading..."));
-    {
-        let response_state = response_state.clone();
-            spawn_local(async move {
-                let result = request_tree().await;
-                let message = match result {
-                    Ok(s) => s,
-                    Err(e) => e.to_string(),
-                };
-                response_state.set(message);
-            });
-    }
-    html! {
-        <>
-        {format!("{:?}", response_state)}
-        </>
-    }
-}
-
-fn main() {
-    yew::Renderer::<App>::new().render();
+        Result::<_, Box<dyn Error>>::Ok(())
+    })
 }
