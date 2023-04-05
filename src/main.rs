d91d9f31c6661e1c8f54dd6282401a4480fad9fa
Evgenii-Barannik <radchemsu@gmail.com>
2023-04-05 04:13:00 +0300

    Continuous updates, better errors, conditional hash
    Also refactored two function components into one.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,77 @@
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(dead_code)]
+use wasm_bindgen::JsCast;
+use web_sys::HtmlInputElement;
+use yew::prelude::*;
 
-mod web;
 mod yaml;
 
+const RAW_YAML_EXAMPLE: &str = include_str!("example.yaml"); // Will be used for the initial state. //***
+
+#[derive(Properties, PartialEq)]
+pub struct Properties {
+    pub name: String,
+    pub value: String,
+}
+
+#[function_component(YamlForm)]
+pub fn yaml_form(properties: &Properties) -> Html {
+    // Loading of the initial state, it latter be used to write the initial value for the textarea:
+    let yaml_state = use_state(|| properties.value.clone());
+
+    let yaml_state_for_callback = yaml_state.clone();
+    let oninput = Callback::from(move |event: InputEvent| {
+        let new_value: String = event
+            .target()
+            .unwrap()
+            .unchecked_into::<HtmlInputElement>()
+            .value();
+
+        yaml_state_for_callback.set(new_value);
+    });
+
+    let input = html! {
+        <form>
+            <textarea rows="20" cols="50" oninput = {oninput} value = {(&*yaml_state).to_string()} />
+            <br/>
+        </form>
+    };
+    let output = match yaml::load_yaml(&*yaml_state) {
+        Ok(yaml_tree) => {
+            let sanitized_yaml = yaml::sanitize_yaml(yaml_tree);
+            let yaml_hash = yaml::hash_yaml(sanitized_yaml.clone());
+            let printed_yaml = yaml::print_yaml(sanitized_yaml);
+
+            html! {
+                <form>
+                    <div style="white-space:pre">{"Yaml is correct 🌟"}</div>
+                    <p>{"Hash: "}<br/>{yaml_hash}</p>
+                    <div style="white-space:pre">{"Sanitized YAML:"}<br/>{printed_yaml}</div>
+                </form>
+            }
+        }
+        Err(error) => {
+            html! {
+                <form>
+                <div style="white-space:pre">{"Yaml is broken ❗"}</div>
+                <p>{"Error: "}<br/>{error}</p>
+                </form>
+            }
+        }
+    };
+
+    html! { <div> {input} {output} </div> }
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let raw_yaml_string = String::from(RAW_YAML_EXAMPLE); //***
+
+    html! {
+        <div>
+        <YamlForm name="yaml_form" value={raw_yaml_string} />
+        </div>
+    }
+}
+
 fn main() {
-    yew::Renderer::<web::App>::new().render();
+    yew::Renderer::<App>::new().render();
 }
--- src/yaml.rs
+++ src/yaml.rs
@@ -1,13 +1,7 @@
 use blake2::{Blake2s256, Digest};
-use hex_literal::hex;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-use std::str;
-use yaml_rust::{Yaml, YamlEmitter};
-use hex::encode;
+use yaml_rust::{Yaml, YamlEmitter, YamlLoader};
 
-pub fn sanitize_tree(mut yaml_to_sanitize: Yaml) -> Yaml {
+pub fn sanitize_yaml(mut yaml_to_sanitize: Yaml) -> Yaml {
     match yaml_to_sanitize {
         Yaml::Real(initial) => {
             let num: f64 = initial.parse().expect("Failed to parse Real.");
@@ -23,13 +17,13 @@ pub fn sanitize_tree(mut yaml_to_sanitize: Yaml) -> Yaml {
         Yaml::Hash(ref mut hashmap_to_modify) => {
             let hashmap_to_traverse = hashmap_to_modify.clone();
             for (key, value) in hashmap_to_traverse.iter() {
-                hashmap_to_modify.insert(key.to_owned(), sanitize_tree(value.clone()));
+                hashmap_to_modify.insert(key.to_owned(), sanitize_yaml(value.clone()));
             }
         }
         Yaml::Array(ref mut vec) => {
             let vec_to_traverse = vec.clone();
             for (i, element) in vec_to_traverse.iter().enumerate() {
-                vec[i] = sanitize_tree(element.clone());
+                vec[i] = sanitize_yaml(element.clone());
             }
         }
         _ => (),
@@ -37,7 +31,7 @@ pub fn sanitize_tree(mut yaml_to_sanitize: Yaml) -> Yaml {
     yaml_to_sanitize
 }
 
-pub fn yaml_to_hash(sanitized_yaml: Yaml) -> String {
+pub fn hash_yaml(sanitized_yaml: Yaml) -> String {
     let mut s = String::new();
     let mut emitter = YamlEmitter::new(&mut s);
     emitter.dump(&sanitized_yaml).unwrap();
@@ -46,4 +40,20 @@ pub fn yaml_to_hash(sanitized_yaml: Yaml) -> String {
     hasher.update(s);
     let hash = hasher.finalize();
     hex::encode(hash)
+}
+
+pub fn load_yaml(read_string: &String) -> Result<Yaml, String> {
+    match YamlLoader::load_from_str(read_string) {
+        Ok(t) => Ok(t[0].clone()),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
+pub fn print_yaml(yaml_to_print: Yaml) -> String {
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(&yaml_to_print).unwrap(); // dump the YAML object to a String
+    }
+    out_str
 }
