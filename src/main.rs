b7436f7eded5564c51a8ee018516b9fb18a590d3
Evgenii-Barannik <radchemsu@gmail.com>
2023-04-05 08:17:52 +0300

    Compiles, but results are not what i wanted

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 use wasm_bindgen::JsCast;
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
+use base64::{Engine, engine::general_purpose};
+use gloo::console::log;
+use reqwasm::http::{Request, Method};
+use wasm_bindgen_futures::spawn_local;
 
 mod yaml;
 
-const RAW_YAML_EXAMPLE: &str = include_str!("example.yaml"); // Will be used for the initial state. //***
+const PROJECT_ID: &str = "2NdtdwZgrdj6fwMubLQMiMTs5nH";
+const PROJECT_SECRET: &str = "78c624acbfe219c5d0b4a8566c867ab0";
+const ENDPOINT: &str = "https://ipfs.infura.io:5001";
+const HASH: &str = "QmfUwJRRDZxGo8jMvKVGxj6FDn8xsMXcyEbRrYaScCXhRv";
+
+async fn request_tree() -> String {
+    let auth_header = format!(
+        "Basic {}",
+        general_purpose::STANDARD.encode(&format!("{}:{}", PROJECT_ID, PROJECT_SECRET))
+    );
+
+    let result = Request::new(&format!("{}/api/v0/cat?arg={}", ENDPOINT, HASH))
+        .method(Method::POST)
+        .header("Authorization", &auth_header)
+        .send()
+        .await;
+
+    match result {
+        Ok(res) => res.text().await.unwrap(),
+        Err(err) => err.to_string(),
+    }
+}
+// const RAW_YAML_EXAMPLE: &str = include_str!("example.yaml"); // Will be used for the initial state. //***
+
 
 #[derive(Properties, PartialEq)]
 pub struct Properties {
@@ -61,13 +88,31 @@ pub fn yaml_form(properties: &Properties) -> Html {
     html! { <div> {input} {output} </div> }
 }
 
-#[function_component(App)]
-pub fn app() -> Html {
-    let raw_yaml_string = String::from(RAW_YAML_EXAMPLE); //***
+// #[function_component(App)]
+// pub fn app() -> Html {
+//     let raw_yaml_string = String::from(RAW_YAML_EXAMPLE); //***
 
+//     html! {
+//         <div>
+//         <YamlForm name="yaml_form" value={raw_yaml_string} />
+//         </div>
+//     }
+// }
+
+#[function_component(App)]
+fn app() -> Html {
+    let response_state = use_state(|| 
+        String::from("Loading..."));
+    {
+        let response_state = response_state.clone();
+            spawn_local(async move {
+                let result = request_tree().await; 
+                response_state.set(result);
+            });
+    }
     html! {
         <div>
-        <YamlForm name="yaml_form" value={raw_yaml_string} />
+        <YamlForm name="yaml_form" value={format!("{:?}", response_state)} />
         </div>
     }
 }
